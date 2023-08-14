5b128daebac018ea6383aa07168ec787e2d95310
mikalai <mikalai.hurynovich@itechart-group.com>
2023-08-14 22:43:09 +0300

    fix: fixed ifinitive procces

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-module.exports = function reverse(n) {
-        let reversed = 0;
-        while (n !== 0) {
-            reversed = reversed * 10 + n % 10;
-            n = Math.floor(n / 10);
-        }
-        return reversed;
+module.exports = function reverse (n) {
+    let str = String(n);
+    let newStr ='';
+    for(let i = str.length; i > 0; i--){
+        newStr = newStr + str[i-1];
     }
+    return parseInt(newStr);
+}
